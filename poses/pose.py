753e885169aec30b06768260f4cc74a2f4ece91d
Fatma Ridaoui <rmham93@gmail.com>
2022-01-10 14:11:28 +0300

    saving poses

--- poses/pose.py
+++ poses/pose.py
@@ -7,56 +7,60 @@ Created on Wed Dec 29 12:01:12 2021
 import cv2
 import mediapipe as mp
 import numpy as np
+import pandas as pd
 import os
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+path = "C:/Users/IDU/Desktop/dataset"
+dir_list = os.listdir(path)
 
-#path = "C:/Users/IDU/Videos/dataset"
-#dir_list = os.listdir(path)
+poses =[]
+header =  []
 
-
-# For static images:
-IMAGE_FILES = ["1001161.jpg"]
-BG_COLOR = (192, 192, 192) # gray
+doc = []
 with mp_pose.Pose(
     static_image_mode=True,
     model_complexity=2,
     enable_segmentation=True,
     min_detection_confidence=0.5) as pose:
-    for idx, file in enumerate(IMAGE_FILES):
-        print(file)
-        path="C:/Users/IDU/Videos/dataset/"+file
-        image = cv2.imread(path)
-        image_height, image_width, _ = image.shape
+    for idx, file in enumerate(dir_list):
+       
+        path = "C:/Users/IDU/Desktop/dataset/"+ file
+        try:
+            image = cv2.imread(path)
+        
+            image_height, image_width, _ = image.shape
+        except:
+            print("error")
         # Convert the BGR image to RGB before processing.
         results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
         if not results.pose_landmarks:
               continue
-        print(
-            f'Nose coordinates: ('
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
-            f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_height})'
-        )
-
-        annotated_image = image.copy()
-        # Draw segmentation on the image.
-        # To improve segmentation around boundaries, consider applying a joint
-        # bilateral filter to "results.segmentation_mask" with "image".
-        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-        bg_image = np.zeros(image.shape, dtype=np.uint8)
-        bg_image[:] = BG_COLOR
-        annotated_image = np.where(condition, annotated_image, bg_image)
-        # Draw pose landmarks on the image.
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        cv2.imwrite('C:/Users/IDU/Videos/pose' + str(idx) + '.png', annotated_image)
-        # Plot pose world landmarks.
-        mp_drawing.plot_landmarks(
-            results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
-
+        
+        
+        line = []
+        for index, each in enumerate(mp_pose.PoseLandmark):
+           
+            if idx ==0:
+                 
+                words = str(each).split(".")
+                if index == 0:
+                    header.append("productid")
+                header.append(words[1]+".x")
+                header.append(words[1]+".y")  
+           
+            if index ==0 :
+                line.append(str(file))
+            line.append(results.pose_landmarks.landmark[each].x * image_width)
+            line.append(results.pose_landmarks.landmark[each].y * image_height)
+        
+        
+        doc.append(line)
+        
+    
+    df = pd.DataFrame(doc, columns=header)
+    print(df.head())
+    df.to_csv('C:/Users/IDU/Desktop/poses_body.csv')  
